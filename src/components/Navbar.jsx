480909beb6a83d59b14c889501c035bccff44bd7
M2DJ Gaming <m2dj2004@gmail.com>
2025-08-03 16:23:34 +0300

    Added Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import img1 from '../assets/React-Logo.webp'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='max-h-20 p-2 flex justify-between bg-orange-700'>
+      <div className=''>
+        <img src={img1} alt='React logo' className='h-15 max-w-15'/>
+      </div>
+      <div className='w-100 max-h-20 pl-3 text-xl font-semibold flex justify-between items-center'>
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Offers</p>
+        <p>Contact Us</p>
+      </div>
+      <div className='w-45 max-h-20 pr-2 text-xl font-semibold flex justify-between items-center'>
+        <p>My account</p>
+        <p>Cart</p>
+      </div>
+    </div>
   )
 }
 
