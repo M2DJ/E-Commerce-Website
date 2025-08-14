45a1fce2515a8344073f30da9c45c76f16a395d2
M2DJ Gaming <m2dj2004@gmail.com>
2025-08-15 02:40:34 +0300

    Fixed error display

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -27,15 +27,17 @@ const Home = () => {
     loadProducts();
   }, []);
 
-  if (error) {
-    return <div></div>;
-  }
-
   console.log(cart);
+  
+  if(error){
+    return <div className="w-full mt-30 flex flex-col items-center">
+      <p className="text-4xl font-bold text-red-500 mb-3">{error}</p>
+      <p className="text-xl">Check back later</p>
+    </div>
+  }
 
   return (
     <div className="p-3 mb-10">
-      {error && <p>{error}</p>}
       <div className="grid grid-cols-5 gap-3 justify-items-center-safe">
         {loading ? (
           <p>Loading...</p>
