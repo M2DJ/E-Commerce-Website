a42b24751f8edfe75636c144de130789e9cb75ea
M2DJ Gaming <m2dj2004@gmail.com>
2025-08-14 17:21:42 +0300

    Finished styling cart with no items

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,8 +1,11 @@
 
 const Cart = () => {
   return (
-    <div>
-        <h1 className="text-red">No items in cart yet</h1>
+    <div className="w-full h-[400px] flex justify-center items-center">
+      <div className="min-w-[450px] h-[250px] text-center rounded-lg bg-gray-300 flex flex-col justify-center">
+        <h1 className="text-red-500 text-4xl font-bold mb-5">No items in cart yet</h1>
+        <p>Start adding items to your cart and you will see them here!</p>
+      </div>
     </div>
   )
 }
