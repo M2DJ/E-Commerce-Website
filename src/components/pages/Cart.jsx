d1f47756cd8817a3c40cfbc41f73c14c61ecc9d9
M2DJ Gaming <m2dj2004@gmail.com>
2025-08-24 14:23:21 +0300

    Fixed cart logic

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,62 +1,75 @@
 import { useCartContext } from "../../context/CartContext";
 
 const Cart = () => {
-  const { cart, deleteFromCart, amount, increaseAmount, decreaseAmount } = useCartContext();
+  const { cart, deleteFromCart, increaseQuantity, decreaseQuantity } =
+    useCartContext();
 
-  const handleDecrease = (e) => {
-    console.log(e.target.id);
-    // if(amount >= 1){
-    //   decreaseAmount();
-    // } else {
-    //   deleteFromCart(e.target.id)
-    // }
+  const handleIncrease = (productId) => {
+    increaseQuantity(productId);
   };
 
-  return cart.map((pro) => (
-    <div className="p-3 flex justify-center">
-      <div className="min-w-200 max-h-36 p-3 flex border rounded-lg">
-        <div className="mr-2">
-          <img className="max-h-30 max-w-30" src={pro.images[0]} />
+  const handleDecrease = (productId) => {
+    var item = cart.find(pro => pro.id === productId);
+
+    if(item.quantity > 1){
+      decreaseQuantity(productId);
+    } else {
+      deleteFromCart(item.id);
+    }
+  };
+
+  if (cart.length == 0) {
+    return (
+      <div className="w-full h-[400px] flex justify-center items-center">
+        <div className="min-w-[450px] min-h-[250px] text-center rounded-lg bg-gray-300 flex flex-col justify-center">
+          <h1 className="text-red-500 text-4xl font-bold mb-5">
+            No items in cart yet
+          </h1>
+          <p>Start adding items to your cart and you will see them here!</p>
         </div>
-        <div>
-          <h1 className="text-xl">{pro.title}</h1>
-          <p className="mb-9 text-lg italic">${pro.price}</p>
-          <div className="max-w-25 flex justify-between">
-            <button
-              className="min-w-6 bg-green-500"
-              onClick={() => increaseAmount()}
-            >
-              +
-            </button>
-            <p className="w-13 border text-center">{amount}</p>
-            <button
-              className="min-w-6 bg-red-500"
-              onClick={handleDecrease}
-            >
-              -
+      </div>
+    );
+  } else {
+    return (
+      <div className="p-3">
+        <div className="flex flex-col items-center">
+          {cart.map((pro) => (
+            <div key={pro.id} className="p-3 flex justify-center">
+              <div className="min-w-200 max-h-36 p-3 flex border rounded-lg">
+                <div className="mr-2">
+                  <img className="max-h-30 max-w-30" src={pro.images[0]} />
+                </div>
+                <div>
+                  <h1 className="text-xl">{pro.title}</h1>
+                  <p className="mb-9 text-lg italic">${pro.price}</p>
+                  <div className="max-w-25 flex justify-between">
+                    <button
+                      className="min-w-6 bg-green-500"
+                      onClick={() => handleIncrease(pro.id)}
+                    >
+                      +
+                    </button>
+                    <p className="w-13 border text-center">{pro.quantity}</p>
+                    <button
+                      className="min-w-6 bg-red-500"
+                      onClick={() => handleDecrease(pro.id)}
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div>
+            <button className="min-w-142 min-h-12 bg-blue-500 text-white border rounded-lg text-2xl">
+              Check Out
             </button>
           </div>
         </div>
       </div>
-    </div>
-  ));
-
-  // if (cart.length == 0) {
-  //   return (
-  //     <div className="w-full h-[400px] flex justify-center items-center">
-  //       <div className="min-w-[450px] min-h-[250px] text-center rounded-lg bg-gray-300 flex flex-col justify-center">
-  //         <h1 className="text-red-500 text-4xl font-bold mb-5">
-  //           No items in cart yet
-  //         </h1>
-  //         <p>Start adding items to your cart and you will see them here!</p>
-  //       </div>
-  //     </div>
-  //   );
-  // } else {
-  //   return <div className="p-3">
-
-  //   </div>;
-  // }
+    );
+  }
 };
 
 export default Cart;
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,44 +1,53 @@
 import { createContext, useContext, useState } from "react";
 
-
 const CartContext = createContext();
 
 export const useCartContext = () => useContext(CartContext);
 
-export const CartProvider = ({children}) => {
-    const [cart, setCart] = useState([]);
-    const [amount, setAmount] = useState(1);
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
-    const addToCart = (product) => {
-        setCart(prev => [...prev, product]);
-    }
+  const addToCart = (product) => {
+    setCart((prev) => [...prev, { ...product, quantity: 1 }]);
+  };
 
-    const deleteFromCart = (productId) => {
-        setCart(prev => prev.filter(pro => pro.id !== productId));
-    }
+  const deleteFromCart = (productId) => {
+    setCart((prev) => prev.filter((pro) => pro.id !== productId));
+  };
 
-    const isInCart = (productId) => {
-        return cart.some(pro => pro.id === productId);
-    }
+  const isInCart = (productId) => {
+    return cart.some((pro) => pro.id === productId);
+  };
 
-    const increaseAmount = () => {
-        setAmount(prev => prev + 1);
-    }
+  const increaseQuantity = (productId) => {
+    setCart((prev) =>
+      prev.map((product) =>
+        product.id === productId
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      )
+    );
+  };
 
-    const decreaseAmount = () => {
-        setAmount(prev => prev - 1);
-    }
+  const decreaseQuantity = (productId) => {
+    setCart((prev) =>
+      prev.map((product) =>
+        product.id === productId
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      )
+    );
+  };
 
-    const functions = {
-        cart,
-        amount,
-        addToCart,
-        deleteFromCart,
-        isInCart,
-        increaseAmount,
-        decreaseAmount
-    }
-    return <CartContext.Provider value={functions}>
-        {children}
-    </CartContext.Provider>
-}
+  const functions = {
+    cart,
+    addToCart,
+    deleteFromCart,
+    isInCart,
+    increaseQuantity,
+    decreaseQuantity,
+  };
+  return (
+    <CartContext.Provider value={functions}>{children}</CartContext.Provider>
+  );
+};
